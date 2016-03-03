d3b2f3346ff99792e40a2a4a69c37a50256a30e3
YangBo <yang.wilby@gmail.com>
2016-03-03 22:10:29 +0100

    Initial Commit

---
+++ myfbswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfbswift
+//
+//  Created by YangBo on 03/03/16.
+//  Copyright © 2016 Symbio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
